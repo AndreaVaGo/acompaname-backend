96cd3f77b2a08460f1b89fffa3c2f4c516bcba23
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-08-27 09:47:26 +0200

    Initial commit: Spring Boot project setup with MySQL, JPA and Security dependencies

---
+++ src/main/java/dev/andrea/acompaname_backend/AcompanameBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.andrea.acompaname_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andrea/acompaname_backend/AcompanameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrea.acompaname_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
